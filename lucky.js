1bc5400510c6274573a6704c89ff29abb5f45aef
Charanaraj N <goudacharanraj760@gmail.com>
2022-12-30 13:40:41 +0530

    initial commit

---
+++ lucky.js
@@ -0,0 +1,7 @@
+const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+const checker = document.querySelector("#check");
+
+checker.addEventListener('click', () => {
+    console.log(luckyNumber.value)
+})
